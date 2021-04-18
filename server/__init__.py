0b84829f6b2d64978807d1c18e8298d8990d780b
Tony <anton.whittaker@gmail.com>
2021-04-18 21:58:43 +0200

    Implemented placeholder functions

--- server/__init__.py
+++ server/__init__.py
@@ -5,23 +5,28 @@ from server.model_utils import load_models, int_to_label_dict
 app = Flask('Text_Classification_Server')
 flask_swagger.swagger(app)
 
-models = load_models()
+models = load_models('/models/')
 
 
 @app.route('/list_models', methods=['GET'])
 def list_models():
     formatted_models = [{'id': i, 'model': model.__repr__()} for i, model in enumerate(models)]
-    return formatted_models
+    return {'models': formatted_models}
 
 
 @app.route('/predict/<id>', methods=['POST'])
 def predict(id):
     assert request.method == 'POST', 'Only POST-call allowed at this endpoint'
-    text = request.body['text']
-    text = text if type(text) == list else [text]
-    values = models[id].predict(text)
+    form = request.form
+    texts = []
+    for key in form.keys():
+        for value in form.getlist(key):
+            if key == 'text':
+                texts.append(value)
+    texts = texts if type(texts) == list else [texts]
+    values = models[int(id)].predict(texts)
     labels = [int_to_label_dict[value] for value in values]
-    return labels
+    return {'labels': labels}
 
 
 # class CustomFlaskApp(object):
--- server/model_utils.py
+++ server/model_utils.py
@@ -1,24 +1,39 @@
 import pathlib
 from glob import glob
 import pickle
-
 from sklearn.pipeline import Pipeline
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# nlp = English()
+#
+#
+# def featurize_text(text):
+#     text = nlp(text)
+#     filtered_text = list()
+#     for word in text:
+#         if (word.is_stop == False) and (word.is_punct == False):
+#             filtered_text.append(word.lemma)
+#     return filtered_text
 
 
 def load_models(directory):
     models = list()
-    dir = pathlib.Path(directory)
-    for file in glob(dir + '*.pkl'):
+    print('.' + directory + '*.pkl')
+    print(glob('.' + directory + '*.pkl'))
+    for file in glob('.' + directory + '*.pkl'):
         with open(file, 'rb') as f:
             pipeline = pickle.load(f)
             if type(pipeline) == Pipeline:
                 models.append(pipeline)
             else:
                 print(f'{file} could not be loaded as a model as it is not a sklearn.pipeline.Pipeline type.')
-
+    return models
 
 int_to_label_dict = {
-    '0': 'negative',
-    '1': 'neutral',
-    '2': 'positive'
+    0: 'negative',
+    1: 'neutral',
+    2: 'positive'
 }
